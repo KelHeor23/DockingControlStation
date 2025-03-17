71bfe1f7b1a7cfa411dad8f591992035607a0863
Alex <kelheor23@gmail.com>
2025-03-17 11:37:37 +0300

    rm build

--- build/Desktop-Debug/CMakeFiles/DockingControlStation_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/DockingControlStation_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/DockingControlStation_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

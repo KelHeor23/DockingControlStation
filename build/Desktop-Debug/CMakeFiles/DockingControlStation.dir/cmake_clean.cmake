476b1aa2c0df3b5583c7638ed7678e50ff18e772
Alex <kelheor23@gmail.com>
2025-03-17 07:42:59 +0300

    Auto stash before merge of "main" and "origin/main"

---
+++ build/Desktop-Debug/CMakeFiles/DockingControlStation.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DockingControlStation_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/DockingControlStation_autogen.dir/ParseCache.txt"
+  "DockingControlStation_autogen"
+  "CMakeFiles/DockingControlStation.dir/DockingControlStation_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/DockingControlStation.dir/DockingControlStation_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/DockingControlStation.dir/main.cpp.o"
+  "CMakeFiles/DockingControlStation.dir/main.cpp.o.d"
+  "DockingControlStation"
+  "DockingControlStation.pdb"
+  "DockingControlStation_autogen/mocs_compilation.cpp"
+  "DockingControlStation_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DockingControlStation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

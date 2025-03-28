9286878ffd520db8660e7b679ff2e6394a907db3
Alex <kelheor23@gmail.com>
2025-03-28 11:39:13 +0300

    interface fix

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    #ifdef Q_OS_WIN
+        a.setWindowIcon(QIcon(":/icins/Resurses/this-is-fine.ico"));  // Windows
+    #else
+        a.setWindowIcon(QIcon(":/icins/Resurses/this-is-fine.png"));  // Linux/Mac
+    #endif
+
     MainWindow w;
     w.show();
     return a.exec();
